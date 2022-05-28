50f3a7dd71cd6f860992737eb9b397a436e47c66
ShadowZAIN <miroshin.gleb@gmail.com>
2022-05-28 13:01:14 +0300

    Lab 1.05.2

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -53,7 +53,15 @@ void cg::renderer::rasterization_renderer::render()
 		rasterizer->set_index_buffer(model->get_index_buffers()[shape_id]);
 		rasterizer->draw(model->get_index_buffers()[shape_id]->get_number_of_elements(), 0);
 	}
-	// TODO: Lab 1.05. Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
+
+	rasterizer->pixel_shader = [](cg::vertex vertex_data, float z) {
+		return cg::color{
+			vertex_data.ambient_r,
+			vertex_data.ambient_g,
+			vertex_data.ambient_b
+		};
+	};
+
 	cg::utils::save_resource(*render_target, settings->result_path);
 }
 
