d2270011ea773f21c125ee727599f4ebe9c9b346
ShadowZAIN <miroshin.gleb@gmail.com>
2022-06-09 23:42:45 +0300

    rasterization

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -48,7 +48,7 @@ void cg::renderer::ray_tracing_renderer::render()
 	raytracer->miss_shader = [](const ray& ray)
 	{
 		payload payload{};
-		payload.color = {0.f, 0.f, (ray.direction.y + 1.f) * 0.5f};
+		payload.color = {(ray.direction.y + 1.f) * 0.5f, 0.f, (ray.direction.y + 1.f) * 0.25f};
 		return payload;
 	};
 
